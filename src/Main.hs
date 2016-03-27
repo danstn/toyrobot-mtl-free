ea9015abf0e21bf010a25791e84d99aad422271f
Daniel S <standeo@gmail.com>
2016-03-27 22:37:41 +1100

    Refactor Free into RobotProgram
    :package:
      - RobotDSL does not need to be parametrized
      - Update signatures with the new RobotProgram alias
      - Rearrange things to prepare for code decoupling

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import           Data.Maybe
+import qualified Data.Map as Map
+import           Linear.V2
 import           Control.Monad.Identity
 import           Control.Monad.Except
 import           Control.Monad.Reader
@@ -7,35 +10,42 @@ import           Control.Monad.Writer
 import           Control.Monad.State
 import           Control.Monad.Free
 
-import           Data.Maybe
-import qualified Data.Map as Map
-
-import           Linear.V2
-
 type Name = String
 type Position = V2 Integer
 type Env = Map.Map String String
 
+-- | Robot data structure
 data Robot = Robot { position :: Position
                    } deriving (Show)
 
+-- | Worlds data structure
 data World = World { robot :: Robot
                    } deriving (Show)
 
-data RobotDSL a next = Place (Integer, Integer) next
+-- | Robot DSL
+data RobotDSL next = Place (Integer, Integer) next
                      | Report next
                      | Done
                      deriving (Functor, Show)
 
-place :: (Integer, Integer) -> Free (RobotDSL a) ()
+
+-- | Robot Program definition and front-end
+--------------------------------------------------------------------------------
+type RobotProgram = Free RobotDSL
+
+place :: (Integer, Integer) -> RobotProgram ()
 place p = liftF $ Place p ()
 
-report :: Free (RobotDSL a) ()
+report :: RobotProgram ()
 report = liftF $ Report ()
 
-done :: Free (RobotDSL a) ()
+done :: RobotProgram ()
 done = liftF Done
+--------------------------------------------------------------------------------
 
+
+-- | Execution monad stack & a runner
+--------------------------------------------------------------------------------
 type Eval a = ReaderT Env (ExceptT String (WriterT [String] (StateT World IO))) a
 
 type EvalStack s m = (MonadIO m
@@ -48,21 +58,31 @@ type EvalResult a = ((Either String a, [String]), World)
 
 runEval :: Env -> World -> Eval a -> IO (EvalResult a)
 runEval env st ev = runStateT (runWriterT (runExceptT (runReaderT ev env))) st
+--------------------------------------------------------------------------------
 
-eval :: forall (m :: * -> *). EvalStack World m => Free (RobotDSL World) () -> m ()
+
+-- | Basic interpreter running in a beefy stack `m`
+--------------------------------------------------------------------------------
+eval :: forall (m :: * -> *). EvalStack World m => RobotProgram () -> m ()
 eval (Free (Report next)) = (liftIO . print =<< get) >> eval next
 eval (Free (Place (x,y) next)) = put (World {robot = Robot {position = V2 x y}}) >> eval next
 eval (Free Done) = return ()
 eval (Pure r) = return r
+--------------------------------------------------------------------------------
 
+
+-- | Sandbox
+--------------------------------------------------------------------------------
 defaultEnv :: Env
 defaultEnv = Map.empty
 
 defaultState :: World
 defaultState = World { robot = Robot { position = V2 0 0 } }
 
-execSandbox :: Free (RobotDSL World) () -> IO (EvalResult ())
+
+execSandbox :: RobotProgram () -> IO (EvalResult ())
 execSandbox = (runEval defaultEnv defaultState) . eval
+--------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
