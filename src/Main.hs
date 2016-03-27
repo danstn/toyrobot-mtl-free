b9b0fc943c3e69956a2763a247db3927f5f0d45a
Daniel S <standeo@gmail.com>
2016-03-27 22:22:58 +1100

    Put the robot in a World
      - Define Robot structure
      - Represent position as a V2 (from linear)
      - Update wiring

--- src/Main.hs
+++ src/Main.hs
@@ -10,16 +10,24 @@ import           Control.Monad.Free
 import           Data.Maybe
 import qualified Data.Map as Map
 
+import           Linear.V2
+
 type Name = String
-type Position = (Integer, Integer)
+type Position = V2 Integer
 type Env = Map.Map String String
 
-data RobotDSL a next = Place a next
+data Robot = Robot { position :: Position
+                   } deriving (Show)
+
+data World = World { robot :: Robot
+                   } deriving (Show)
+
+data RobotDSL a next = Place (Integer, Integer) next
                      | Report next
                      | Done
                      deriving (Functor, Show)
 
-place :: a -> Free (RobotDSL a) ()
+place :: (Integer, Integer) -> Free (RobotDSL a) ()
 place p = liftF $ Place p ()
 
 report :: Free (RobotDSL a) ()
@@ -28,7 +36,7 @@ report = liftF $ Report ()
 done :: Free (RobotDSL a) ()
 done = liftF Done
 
-type Eval a = ReaderT Env (ExceptT String (WriterT [String] (StateT Position IO))) a
+type Eval a = ReaderT Env (ExceptT String (WriterT [String] (StateT World IO))) a
 
 type EvalStack s m = (MonadIO m
                     , MonadState s m
@@ -36,19 +44,25 @@ type EvalStack s m = (MonadIO m
                     , MonadReader Env m
                     , MonadWriter [Name] m)
 
-type EvalResult a = ((Either String a, [String]), Position)
+type EvalResult a = ((Either String a, [String]), World)
 
-runEval :: Env -> Position -> Eval a -> IO (EvalResult a)
+runEval :: Env -> World -> Eval a -> IO (EvalResult a)
 runEval env st ev = runStateT (runWriterT (runExceptT (runReaderT ev env))) st
 
-eval :: forall (m :: * -> *). EvalStack Position m => Free (RobotDSL Position) () -> m ()
+eval :: forall (m :: * -> *). EvalStack World m => Free (RobotDSL World) () -> m ()
 eval (Free (Report next)) = (liftIO . print =<< get) >> eval next
-eval (Free (Place pos next)) = put pos >> eval next
+eval (Free (Place (x,y) next)) = put (World {robot = Robot {position = V2 x y}}) >> eval next
 eval (Free Done) = return ()
 eval (Pure r) = return r
 
-execSandbox :: Free (RobotDSL Position) () -> IO (EvalResult ())
-execSandbox = (runEval Map.empty (0, 0)) . eval
+defaultEnv :: Env
+defaultEnv = Map.empty
+
+defaultState :: World
+defaultState = World { robot = Robot { position = V2 0 0 } }
+
+execSandbox :: Free (RobotDSL World) () -> IO (EvalResult ())
+execSandbox = (runEval defaultEnv defaultState) . eval
 
 main :: IO ()
 main = do
